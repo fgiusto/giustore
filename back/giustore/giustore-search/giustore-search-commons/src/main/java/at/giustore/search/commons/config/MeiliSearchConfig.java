f707c5f4892e78823c32bc3944e3d9490f8255d8
Fred Giusto <frederic.giusto@gmail.com>
2026-01-23 21:43:09 +0100

    Meilisearch config

---
+++ back/giustore/giustore-search/giustore-search-commons/src/main/java/at/giustore/search/commons/config/MeiliSearchConfig.java
@@ -0,0 +1,16 @@
+package at.giustore.search.commons.config;
+
+import com.meilisearch.sdk.Client;
+import com.meilisearch.sdk.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MeiliSearchConfig {
+
+    @Bean
+    public Client meiliClient() {
+        return new Client(new Config("http://localhost:7700", "giust0reLive"));
+    }
+
+}
